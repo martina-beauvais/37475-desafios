827c94a2df47e8baccc09b3798a81e456bd3dc74
Martina Beauvais <marbeauvais17@gmail.com>
2022-06-28 08:38:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let i = parseInt (prompt("Ingresar un número entre 1 y 100 para sumar 10"));
+do{
+    i = i + 10;
+    alert("La suma da "+i+" y si le sumo 10 más")
+} while(i <100){
+    alert("¡Gracias!");
+}
