af12c50b0ffc27378c9079b145a5f10f78cf8229
Martina Beauvais <marbeauvais17@gmail.com>
2022-07-20 12:24:01 -0300

    Añadiendo eventos para el desafio de la clase 9

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-//Desafio complementario 3 
+//DESAFIO 3//
+class Usuario {
+    constructor(username, email, contraseña){
+        this.username = username,
+        this.email = email, 
+        this.contraseña = contraseña
+        
+    }
+}
+
+const usuarios = []
+
+const form = document.getElementById("idForm")
+const usuariocreado = document.getElementById("usuarios")
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    let username = document.getElementById("inlineFormInputGroupUsername").value
+    let email = document.getElementById("inputEmail4").value
+    let contraseña = document.getElementById("inputPassword4").value
+
+    const usuario = new Usuario(username, email, contraseña)
+
+    usuarios.push(usuario)
+    console.log(usuarios)
+
+    form.reset()
+})
+
+
+
+/*Desafio complementario 3 
 class Libros {
     constructor(id, nombre, autor, precio, imagen ){
         this.id = id,
@@ -40,7 +73,9 @@ libros.forEach(librosArray => {
         </div>
     </div>
   `
-})
+})*/
+
+
 
 
 /*Desafio complementario 2//
